84b7958bfe3989086f1bf9a2f6218b83fdeb5442
HtetO2Ko <hteto2ko.dev@gmail.com>
2026-09-14 10:41:53 +0630

    add quiz complete summary and shuffleAnswers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import QuizCompleteImage from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAns) {
     setUserAnswers((prev) => {
@@ -12,12 +14,25 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizCompleteImage} alt={QuizCompleteImage} />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  // eslint-disable-next-line react-hooks/purity
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((ans) => (
+          {shuffledAnswers.map((ans) => (
             <li key={ans} className="answer">
               <button onClick={() => handleSelectAnswer(ans)}>{ans}</button>
             </li>
